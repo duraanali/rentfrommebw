7627960457c10a98c8e143b0a999580e647db9cf
Julie <julieantonio38@gmail.com>
2019-10-22 19:11:15 -0400

    switched to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,29 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+    client: 'pg',
     connection: {
-      filename: './data/usemytools.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
+      database: "usemytools",
+      user: "postgres",
+      password: "mullen123"
     },
     migrations: {
       directory: './data/migrations',
+      tableName: "knex_migrations"
     },
     seeds: {
       directory: './data/seeds',
     },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "knex_migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
   }
 }
